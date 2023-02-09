9ff3cb72451758dcf232d7e4b3a93b4bc474a0d4
Stephen Heinen <stephenheinen22@yahoo.com>
2023-02-09 02:05:00 -0600

    got a little narrow sighted, almost all components are complete and useState added for open and close cart

---
+++ src/store/CartProvider.js
@@ -0,0 +1,22 @@
+import CartContext from "./cart-context";
+
+function CartProvider(props) {
+  const addItemToCartHandler = (item) => {
+
+  };
+
+  const removeItemFromCartHandler = (id) => {
+
+  };
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return <CartContext.Provider>{props.children}</CartContext.Provider>;
+}
+
+export default CartProvider;
